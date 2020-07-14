2a9dbdf8c0fecd50e0dbe5e50207ca195d997187
HarroJongen <harro.jongen@wur.nl>
2020-07-14 20:24:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,25 +4,17 @@
 #Excecutes the complete proces to analyze the connections of drought and UHI for NL and BE
 
 #Import packages
-import os
-import inspect
 import datetime
 import pandas as pd
-
-
-#Set source file location as working directory
-module_path = inspect.getfile(inspect.currentframe())
-module_dir = os.path.realpath(os.path.dirname(module_path))
-os.chdir(module_dir)
 
 #Import functions
 from Functions import SICFormatter
 from Functions import WOWFormatter
 from Functions import UrbanRuralCombiner
-#from Functions import UrbanRuralFormatter
+from Functions import UrbanRuralFormatter
 
 #Select period and format Amsterdata from Summer in the City to project's format
-#SICFormatter.SICFormatter(start = datetime.datetime(2017, 1, 1), end = datetime.datetime(2018 , 12, 31), file = "Data/5G0D2194(5G0D2194)-1554817879612.xlsx")
+SICFormatter.SICFormatter(start = datetime.datetime(2017, 1, 1), end = datetime.datetime(2018 , 12, 31), file = "Data/5G0D2194(5G0D2194)-1554817879612.xlsx")
 
 #Format WOW-NL data
 WOWFormatter.WOWFormatter('Amsterdam', start = datetime.datetime(2017, 1, 1),\
@@ -32,5 +24,8 @@ WOWFormatter.WOWFormatter('Amsterdam', start = datetime.datetime(2017, 1, 1),\
 #Create csv with rural and urban station combinations
 UrbanRuralCombiner.UrbanRuralCombiner()
 
-#
-
+#Create combined datasets
+Combinations = pd.read_csv('Data/UrbanRuralCombinations.csv')
+for item in range(len(Combinations)):
+    UrbanRuralFormatter.UrbanRuralFormatter(Combinations['City'][item], Combinations['UrbanFile'][item], Combinations['RuralFile'][item])
+    
