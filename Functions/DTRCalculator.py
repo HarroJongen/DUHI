820a7bf698c908f2506a0980b6312379fe999a80
HarroJongen <harro.jongen@wur.nl>
2020-07-16 16:36:37 +0200

    Create DTRCalculator.py

---
+++ Functions/DTRCalculator.py
@@ -0,0 +1,8 @@
+#Title: DTR calculator
+#Date: 10-07-2020
+#Author: Harro Jongen
+#Calculates the range to define diurnal temperture range
+
+def DTRCalculator(array):
+    return (max(array)-min(array))
+ 
