b1f69bda94e179f4cb8c0d57cd4685de39127cb6
HarroJongen <harro.jongen@wur.nl>
2020-07-16 16:36:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,29 +3,50 @@
 #Author: Harro Jongen
 #Excecutes the complete proces to analyze the connections of drought and UHI for NL and BE
 
+###IMPORTING###
+
 #Import packages
 import datetime
 import pandas as pd
+import glob
 
 #Import functions
-from Functions import SICFormatter
-from Functions import WOWFormatter
-from Functions import UrbanRuralCombiner
-from Functions import UrbanRuralFormatter
+from Functions import SICFormatter, WOWFormatter, RdamFormatter, UrbanRuralCombiner, UrbanRuralFormatter, DailyBuilder
+
+#Define period of interest
+start = datetime.datetime(2017, 1, 1)
+end = datetime.datetime(2018 , 12, 31)
+
+###FORMATTING DATA###
 
 #Select period and format Amsterdata from Summer in the City to project's format
-SICFormatter.SICFormatter(start = datetime.datetime(2017, 1, 1), end = datetime.datetime(2018 , 12, 31), file = "Data/5G0D2194(5G0D2194)-1554817879612.xlsx")
+SICFormatter.SICFormatter(start, end, file = "Data/5G0D2194(5G0D2194)-1554817879612.xlsx")
 
 #Format WOW-NL data
-WOWFormatter.WOWFormatter('Amsterdam', start = datetime.datetime(2017, 1, 1),\
-                          end = datetime.datetime(2018 , 12, 31), file = "Data/export_916696001.csv")
+WOWFormatter.WOWFormatter('Amsterdam', start, end , file = "Data/Amsterdam/export_916696001.csv")
     
-
+#Format Rotterdam data
+Files = glob.glob('Data/Rotterdam/*.csv')
+for item in Files:
+    RdamFormatter.RdamFormatter(start, end, item)
+ 
 #Create csv with rural and urban station combinations
 UrbanRuralCombiner.UrbanRuralCombiner()
 
 #Create combined datasets
 Combinations = pd.read_csv('Data/UrbanRuralCombinations.csv')
 for item in range(len(Combinations)):
-    UrbanRuralFormatter.UrbanRuralFormatter(Combinations['City'][item], Combinations['UrbanFile'][item], Combinations['RuralFile'][item])
-    
+    UrbanRuralFormatter.UrbanRuralFormatter(Combinations['City'][item], Combinations['UrbanFile'][item],\
+                                            Combinations['RuralFile'][item])
+
+#Calculate matrics on daily basis
+for file in Combinations['TotalFile']:
+    DailyBuilder.DailyBuilder(file, k_API = 0.85)
+
+###ANALYSIS###
+
+
+
+
+
+###VISUALIZATION###
