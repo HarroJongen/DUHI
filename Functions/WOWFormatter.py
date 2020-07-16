91577b5b838fb6a358cfb7bb95cc65a8e4d9389f
HarroJongen <harro.jongen@wur.nl>
2020-07-16 16:37:16 +0200

    Included both urban and rural

--- Functions/WOWFormatter.py
+++ Functions/WOWFormatter.py
@@ -3,19 +3,24 @@
 #Author: Harro Jongen
 #Formats data of WOW-nl to csv
 
-def WOWFormatter(city, start, end, file = "Data/export_916696001.csv"):
+def WOWFormatter(city, start, end, file, rural = True):
     import pandas as pd
     import numpy as np
     
     #Read WOW file
     df = pd.read_csv(file, delimiter = ';')
     
+    #Set right suffix for location
+    if rural:
+        Loc = 'rural'
+    else:
+        Loc = 'urban'
     
-    #Convert dates to datetime format ####FORMAT WRONG#####
+    #Convert dates to datetime format
     df['date'] = pd.to_datetime(df['datum'], format='%Y-%m-%dT%H:%M:%S')
     
     #Rename columns
-    df.rename({'temperatuur (C) [916696001]' : 'T_rural', 'neerslagintensiteit (mm/uur) [916696001]' : 'P_rural', 'windsnelheid (m/s) [916696001]' : 'u_rural', 'relatieve vochtigheid  (%) [916696001]' : 'RH_rural', 'luchtdruk (hPa) [916696001]' : 'p_rural'}, axis = 1, inplace = True)
+    df.rename({'temperatuur (C) [916696001]' : 'T_' + Loc, 'neerslagintensiteit (mm/uur) [916696001]' : 'P_' + Loc, 'windsnelheid (m/s) [916696001]' : 'u_' + Loc, 'relatieve vochtigheid  (%) [916696001]' : 'RH_' + Loc, 'luchtdruk (hPa) [916696001]' : 'p_' + Loc}, axis = 1, inplace = True)
     
     #Select period
     df = df[(df['date'] > start) &(df['date'] < end)]
@@ -24,6 +29,6 @@ def WOWFormatter(city, start, end, file = "Data/export_916696001.csv"):
     df = df.replace('-',np.NaN)
     
     #Write to csv file
-    filepath = 'Data/' + city + '_rural.csv'
+    filepath = 'Data/' + city + '_' + Loc + '.csv'
     df.to_csv(filepath, columns = (['date', 'T_rural', 'u_rural', 'P_rural', 'p_rural', 'RH_rural']), index = False)
         
