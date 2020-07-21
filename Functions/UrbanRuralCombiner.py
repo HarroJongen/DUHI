a859b712399d3989a324f2b15a9a64ec81e6bb17
HarroJongen <harro.jongen@wur.nl>
2020-07-21 15:01:56 +0200

    Check file existence

--- Functions/UrbanRuralCombiner.py
+++ Functions/UrbanRuralCombiner.py
@@ -7,6 +7,8 @@ def UrbanRuralCombiner():
     import glob
     import re
     import pandas as pd
+    import os.path
+    
     #Initialize empty dataframe for rural and urban station combinations
     Locations = pd.DataFrame(columns = ['City', 'UrbanFile', 'RuralFile', 'TotalFile'])
     #Read all filenames for rural and urban sites
@@ -14,13 +16,18 @@ def UrbanRuralCombiner():
     
     #Create regular expression to scan filenames for city names
     regex_city = re.compile(r'Data\\([A-Za-z]+)_\w+.csv')
+    
     #For every urban file
     for item in range(len(UrbanFiles)):
         UrbanFile = UrbanFiles[item]
         City = regex_city.findall(UrbanFiles[item])
         RuralFile = 'Data\\' + City[0] + '_rural.csv'
         TotalFile = UrbanFiles[item][0:-9] + 'total.csv'
-        Locations.loc[item] = [City[0]] + [UrbanFile] + [RuralFile] + [TotalFile]
+        
+        #Check whether rural file is available
+        if os.path.isfile(RuralFile):
+            #Add combination to the dataframe
+            Locations.loc[item] = [City[0]] + [UrbanFile] + [RuralFile] + [TotalFile]
     
     #Write dataframe to csv
     Locations.to_csv('Data\\UrbanRuralCombinations.csv', index = False)
