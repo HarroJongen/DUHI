5ff4326e010d04e0166e1d1a4aea27a34f65f2f6
HarroJongen <harro.jongen@wur.nl>
2020-08-25 14:55:00 +0200

    Create GlobalMapSoilMoisture.py

---
+++ Functions/GlobalMapSoilMoisture.py
@@ -0,0 +1,21 @@
+#Title: GlobalMapSoilMoisture
+#Date: 23-07-2020
+#Author: Harro Jongen
+#Draws a global map of the soil moisture for a given filepath
+
+def GlobalMapSoilMoisture(filepath):
+    from netCDF4 import Dataset as NetCDFFile 
+    import numpy as np
+    from mpl_toolkits.basemap import Basemap
+    
+    nc = NetCDFFile(filepath)
+    lats = nc.variables['lat'][:]
+    lons = nc.variables['lon'][:]
+    sm = nc.variables['sm'][:]
+    nc.close()
+    m = Basemap(projection='kav7',lon_0=0,resolution='l')
+    lon, lat = np.meshgrid(lons, lats)
+    xi, yi = m(lon, lat)
+    m.pcolor(xi,yi,np.squeeze(sm))
+    m.drawcountries()
+    m.drawcoastlines()
