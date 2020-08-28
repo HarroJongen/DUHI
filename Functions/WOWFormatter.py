bea316ec7f1218766814c40898e37365c8d62bea
HarroJongen <harro.jongen@wur.nl>
2020-08-28 15:12:55 +0200

    Check file existence

--- Functions/WOWFormatter.py
+++ Functions/WOWFormatter.py
@@ -7,15 +7,16 @@ def WOWFormatter(city, station, start, end, file, rural = True, overwrite = True
     import pandas as pd
     import numpy as np
     import os.path
+    import datetime
     
-    #Set right suffix for location
+    #Set right suffix for location and define  saving filename
     if rural:
         Loc = 'rural'
+        filepath = 'Data/' + city + '_' + Loc + '.csv'
     else:
         Loc = 'urban'
-        
-    #Define saving filename
-    filepath = 'Data/' + city + '_' + Loc + '.csv'
+        filepath = 'Data/' + city + '_' + station + '_' + Loc + '.csv'
+
     #Check whether file does not exists or whether it should be overwritten
     if overwrite or not os.path.isfile(filepath):
         
@@ -24,6 +25,8 @@ def WOWFormatter(city, station, start, end, file, rural = True, overwrite = True
                 
         #Convert dates to datetime format
         df['date'] = pd.to_datetime(df['datum'], format='%Y-%m-%dT%H:%M:%S')
+        #Convert time to UTC
+        df['date'] = df['date'] - datetime.timedelta(hours=2)
         
         #Rename columns
         df.rename({'temperatuur (C) ['+ station +']' : 'T_' + Loc, 'neerslagintensiteit (mm/uur) ['+ station +']' : 'P_' + Loc, 'windsnelheid (m/s) ['+ station +']' : 'u_' + Loc, 'relatieve vochtigheid  (%) ['+ station +']' : 'RH_' + Loc, 'luchtdruk (hPa) ['+ station +']' : 'p_' + Loc}, axis = 1, inplace = True)
@@ -34,6 +37,8 @@ def WOWFormatter(city, station, start, end, file, rural = True, overwrite = True
         #Check if data is available in given period
         if len(df) == 0 :
             print('No data available between ' + str(start) + ' and ' + str(end) + ' for ' + Loc + ' WOW location of ' + city)
+            if os.path.isfile(filepath):
+                os.remove(filepath)
         else:
             #Replace - with NA
             df = df.replace('-',np.NaN)
