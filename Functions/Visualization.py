315bf69ff8f5274c8f2b4ae479dda0468c84a28a
HarroJongen <harro.jongen@wur.nl>
2020-09-17 14:51:42 +0200

    Add non subset scatters

--- Functions/Visualization.py
+++ Functions/Visualization.py
@@ -29,6 +29,96 @@ def Boxplot(cat, dataframe, analysis_periodtype, analysis_date):
     plt.savefig('Figures/Boxplots_' + cat + '_' +  analysis_periodtype + '_' + analysis_date)
     plt.close()
 
+def Scatter(cat, dataframe, analysis_name):
+    import matplotlib.pyplot as plt
+    
+    fig, axes = plt.subplots(figsize=(20,10), nrows=1, ncols=2)
+    
+    axes[0].scatter(dataframe['sm_cor'], dataframe[cat])
+    axes[0].set_ylabel(cat)
+    axes[0].set_xlabel('Soil moisture')
+  
+    axes[1].scatter(dataframe['API0.85_rural'], dataframe[cat])
+    axes[1].set_xlabel('Antecedent precipitation index (k = 0.85)')
+    
+    fig.suptitle('Scatter ' + cat + ' against moisture proxies')
+    
+    plt.savefig('Figures/Scatter_' + cat + '_SM_' +  analysis_name)
+    plt.close()
+
+def ScatterCity(cat1, cat2, dataframe, analysis_name):
+    import matplotlib.pyplot as plt
+    
+    fig, axes = plt.subplots(figsize=(20,10), nrows=2, ncols=2)
+    
+    axes[0,0].scatter(dataframe[cat2], dataframe[cat1 ])
+    axes[0,0].set_ylabel(cat1)
+    axes[0,0].set_title('All cities')
+  
+    axes[0,1].scatter(dataframe[dataframe['City'] == 'Amsterdam'][cat2], dataframe[dataframe['City'] == 'Amsterdam'][cat1])
+    axes[0,1].set_title('Amsterdam')
+
+    axes[1,0].scatter(dataframe[dataframe['City'] == 'Rotterdam'][cat2], dataframe[dataframe['City'] == 'Rotterdam'][cat1])
+    axes[1,0].set_ylabel(cat1)
+    axes[1,0].set_xlabel(cat2)
+    axes[1,0].set_title('Rotterdam')
+
+    axes[1,1].scatter(dataframe[dataframe['City'] == 'Gent'][cat2], dataframe[dataframe['City'] == 'Gent'][cat1])
+    axes[1,1].set_xlabel(cat2)
+    axes[1,1].set_title('Gent')
+
+    
+    fig.suptitle('Scatter ' + cat1 + ' against ' + cat2 + ' per city')
+    
+    plt.savefig('Figures/Scatter_' + cat1 + '_' + cat2[0] + '_' +  analysis_name)
+    plt.close()
+    
+def ScatterCitySM(cat1, dataframe, analysis_name):
+    import matplotlib.pyplot as plt
+    
+    fig, axes = plt.subplots(figsize=(20,10), nrows=2, ncols=2)
+    
+    axes[0,0].scatter(dataframe['sm_cor'], dataframe[cat1 ])
+    axes[0,0].set_ylabel(cat1)
+    axes[0,0].set_title('All cities')
+  
+    axes[0,1].scatter(dataframe[dataframe['City'] == 'Amsterdam']['sm'], dataframe[dataframe['City'] == 'Amsterdam'][cat1])
+    axes[0,1].set_title('Amsterdam')
+
+    axes[1,0].scatter(dataframe[dataframe['City'] == 'Rotterdam']['sm'], dataframe[dataframe['City'] == 'Rotterdam'][cat1])
+    axes[1,0].set_ylabel(cat1)
+    axes[1,0].set_xlabel('sm')
+    axes[1,0].set_title('Rotterdam')
+
+    axes[1,1].scatter(dataframe[dataframe['City'] == 'Gent']['sm'], dataframe[dataframe['City'] == 'Gent'][cat1])
+    axes[1,1].set_xlabel('sm')
+    axes[1,1].set_title('Gent')
+
+    
+    fig.suptitle('Scatter ' + cat1 + ' against sm per city')
+    
+    plt.savefig('Figures/Scatter_' + cat1 + '_sm_' +  analysis_name)
+    plt.close()
+
+def ScatterSelect(cat, cat_select, select, dataframe, analysis_name):
+    import matplotlib.pyplot as plt
+    
+    fig, axes = plt.subplots(figsize=(20,10), nrows=1, ncols=2)
+    
+    axes[0].scatter(dataframe[dataframe[cat_select] == select]['sm'], dataframe[dataframe[cat_select] == select][cat])
+    axes[0].set_ylabel(cat)
+    axes[0].set_xlabel('Soil moisture')
+  
+    axes[1].scatter(dataframe[dataframe[cat_select] == select]['API0.85_rural'], dataframe[dataframe[cat_select] == select][cat])
+    axes[1].set_xlabel('Antecedent precipitation index (k = 0.85)')
+    
+    fig.suptitle('Scatter ' + cat + ' against moisture proxies for ' + cat_select + ' is ' + select)
+    
+    plt.savefig('Figures/Scatter_' + cat + '_' + select + '_SM_' +  analysis_name)
+    plt.close()
+
+#%% Subset plots
+
 def ScatterSubset(cat, dataframe, dataframe_sub, analysis_name):
     import matplotlib.pyplot as plt
     
